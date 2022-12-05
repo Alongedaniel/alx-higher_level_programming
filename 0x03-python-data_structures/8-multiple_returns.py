b1b252f2a748a923b29f4621ee048e2830145d00
Alongedaniel <alongedaniel41@gmail.com>
2022-12-05 06:41:59 -0800

    Added tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     if sentence != '':
         first = sentence[0]
-        return(len(sentence), first)
     else:
         first = None
+    return(len(sentence), first)
