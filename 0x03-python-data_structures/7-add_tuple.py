227970e201f9677cb7f4be6df826ecadc7eddb39
Alongedaniel <alongedaniel41@gmail.com>
2022-12-05 07:05:28 -0800

    Added tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,4 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    return tuple(sum(i) for i in zip((tuple_a), (tuple_b)))
+    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return added_tuple
